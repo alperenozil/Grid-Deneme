864409af17dbb72bf9ef1a150519f44d0298cef8
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-09 21:06:25 +0300

    Initial Commit

---
+++ Grid Deneme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grid Deneme
+//
+//  Created by Necdet Alperen Özil on 9.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
